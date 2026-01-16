d4e7e571cbe358e161d7880706a5d7e2e763b700
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-01-16 06:49:09 -0600

    exists method added to Trie class

--- build_tries.py
+++ build_tries.py
@@ -25,6 +25,24 @@ Example Tree:
 
 class Trie:
 
+    #method to check if the given word exists in the tree
+    def exists(self,word):
+
+        current_dict = self.root
+
+        for c in word:
+            if c not in current_dict:
+                return False
+            
+            current_dict = current_dict[c]
+            
+            if self.end_symbol in current_dict:
+                return True
+            
+            else:
+                return False
+
+
     #method to insert into a tree
     def insert(self,word):
 
