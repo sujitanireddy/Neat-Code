5e8a5c6cc0317c2a612f66d81de87d6e11a396f3
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-01-16 07:24:48 -0600

    added search fucntions to tries

--- build_tries.py
+++ build_tries.py
@@ -60,3 +60,39 @@ class Trie:
         self.root = {}
         self.end_symbol = '*'
 
+
+    #This function collects all complete words starting from the current trie level. It takes the current dictionary level, the accumulated prefix so far, and the collection of words found.
+    def search_level(self, current_level, current_prefix, words):
+
+        if self.end_symbol in current_level:
+            words.append(current_prefix)
+
+        current_level_keys = current_level.keys()
+        sorted_current_level_keys = sorted(current_level_keys)
+        
+        for char in sorted_current_level_keys:
+
+            if char == self.end_symbol:
+                continue
+
+            new_current_prefix = current_prefix + char
+
+            self.search_level(current_level[char], new_current_prefix, words)
+
+        return words
+            
+    #his finds all words in the trie that begin with a given prefix.
+    def words_with_prefix(self, prefix):
+
+        matching_words = []
+
+        current_level = self.root
+
+        for c in prefix:
+
+            if c not in current_level:
+                return matching_words
+
+            current_level = current_level[c]
+
+        return self.search_level(current_level, prefix, matching_words)
