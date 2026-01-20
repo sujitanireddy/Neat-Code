0f2362bc15fce6c73eb21ae0b046462b6012b85e
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-01-20 08:06:05 -0600

    adjacency list function added to graph class

---
+++ graph.py
@@ -0,0 +1,35 @@
+#Building Graph from scratch as part of boot.dev course work
+
+class Graph:
+    def __init__(self, num_vertices):
+        self.graph = []
+
+        for i in range(num_vertices):
+            self.graph.append([])
+            for j in range(num_vertices):
+                self.graph[i].append(False)
+
+    def add_edge(self, u, v):
+        self.graph[u][v] = True
+        self.graph[v][u] = True
+
+    def edge_exists(self, u, v):
+        if u < 0 or u >= len(self.graph):
+            return False
+        if len(self.graph) == 0:
+            return False
+        row1 = self.graph[0]
+        if v < 0 or v >= len(row1):
+            return False
+        return self.graph[u][v]
+    
+    #Function to build graph using adjacency list and not matrix
+    def add_edge(self, u, v):
+        if u in self.graph.keys():
+            self.graph[u].add(v)
+        else:
+            self.graph[u] = set([v])
+        if v in self.graph.keys():
+            self.graph[v].add(u)
+        else:
+            self.graph[v] = set([u])
