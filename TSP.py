1eedcfd264107fc007cc08045021e68a21d544c6
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-01-21 21:51:46 -0600

    TSP problem solved (Brute Force)

---
+++ TSP.py
@@ -0,0 +1,37 @@
+#Travelling Salesman Problem
+
+def tsp(cities, paths, dist):
+
+    possible_paths = permutations(cities)
+
+    for possible_path in possible_paths:
+
+        total_distance = 0
+
+        for i in range(len(possible_path) - 1):
+
+            total_distance += paths[possible_path[i]][possible_path[i+1]]
+
+        if total_distance < dist:
+            return True
+
+    return False
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
