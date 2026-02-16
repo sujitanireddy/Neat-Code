caa107f5d899f3786462ba158ba9ab5db715fc4f
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-02-16 15:15:40 -0600

    binary search question solved

---
+++ binary-search.py
@@ -0,0 +1,22 @@
+#Question -> https://neetcode.io/problems/binary-search/question?list=neetcode150
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        L, R = 0, len(nums) - 1
+
+        while L <= R:
+
+            M = (L + R) // 2
+
+            if nums[M] < target:
+                L = M + 1
+
+            elif nums[M] > target:
+                R = M - 1
+
+            else:
+                return M
+
+        return -1
+        
