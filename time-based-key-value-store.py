8e3fbb7ad734c2125704ba772784cae705c5da7b
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-02-19 07:58:16 -0600

    solved time-based-key-value-store problem optimally

--- time-based-key-value-store.py
+++ time-based-key-value-store.py
@@ -2,7 +2,8 @@
 
 import collections
 
-class TimeMap:
+#Bute Force: Time Complexity: O(n) and Space Complexity: O(n)
+class TimeMap_Brute:
 
     def __init__(self):
         self.keystore = collections.defaultdict(list)
@@ -24,6 +25,38 @@ class TimeMap:
                     seen_value = value_time[0]
         return seen_value
             
+#Optimal Way: Time Complexity: O(log n) and Space Complexity: O(n)
+class TimeMap:
+
+    def __init__(self):
+        self.keystore = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.keystore[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        if key not in self.keystore:
+            return ""
+    
+        else:
+            arr = self.keystore[key]
+            l = 0
+            r = len(arr) - 1
+            result = ""
+
+            while l <= r:
+
+                mid = (l+r) // 2
+                
+                if arr[mid][1] <= timestamp:
+                    result = arr[mid][0]
+                    l = mid + 1
+                
+                else:
+                    r = mid - 1
+            
+            return result
             
                 
 
@@ -36,15 +69,16 @@ class TimeMap:
 
 
 
-
+#testing
 timemap = TimeMap()
-#timemap.set(key="alice", value="happy", timestamp=1)
+
 timemap.set(key="alice", value="happy", timestamp=1)
 timemap.set(key="alice", value="sad", timestamp=3)
 timemap.set(key="alice", value="mad", timestamp=4)
 timemap.set(key="alice", value="tired", timestamp=5)
 timemap.set(key="alice", value="super happy", timestamp=6)
 timemap.set(key="alice", value="very sad", timestamp=7)
+
 
 get_result = timemap.get(key="alice", timestamp=100)
 print(get_result)
