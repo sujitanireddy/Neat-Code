28bddc5b610f344cfd771bbf84c0b8d0f7207d6b
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-02 11:02:18 -0600

    palildrome revised

--- palindrome.py
+++ palindrome.py
@@ -16,3 +16,22 @@ def is_palindrome(word):
     return True
 
 print(is_palindrome(word=""))
+
+
+#Valid Palindrome the regular way. Can also use list slicing[::-1] to reverse a string.
+def isPalindrome(self, s: str) -> bool:
+
+    reversed_s = ''
+    for i in range(len(s) - 1, -1 , -1):
+        if s[i].isalnum():
+            reversed_s += s[i]
+
+    stripped_s = ''
+    for i in range(len(s)):
+        if s[i].isalnum():
+            stripped_s += s[i]
+        
+    if stripped_s.lower() == reversed_s.lower():
+        return True
+    else:
+        return False
