f8643f2446da28db688dc90a9ff699381681e4fe
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-03 09:10:52 -0600

    three sum solved again on March 3rd 2026

--- three_sum.py
+++ three_sum.py
@@ -84,3 +84,43 @@ Algorithm:
             - Using a while loop make sure L and R pointers are skipped if they are duplicates. i.e while L < R and nums[L] == nums[L-1] move L pointer. Same with right pointer.
 """
 
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        triplets = []
+
+        nums.sort()
+
+        for i, a in enumerate(nums):
+
+            if a > 0:
+                break
+                
+            if i > 0 and a == nums[i-1]:
+                continue
+
+            L = i + 1
+            R = len(nums) - 1
+
+            while L < R:
+
+                if a + nums[L] + nums[R] == 0:
+                    triplets.append([a, nums[L], nums[R]])
+                    L += 1
+                    R -= 1
+
+                    while L < R and nums[L] == nums[L-1]:
+                        L += 1
+                    
+                    while L < R and nums[R] == nums[R+1]:
+                        R -= 1
+                
+                elif a + nums[L] + nums[R] > 0:
+                    R -= 1
+                
+                else:
+                    L += 1
+        
+        return triplets
+
+
