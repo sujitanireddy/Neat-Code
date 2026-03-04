9fd1e6f18299ca3e3bed57def3251018777f8350
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-04 09:19:43 -0600

    Revised on March 4th 2026

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -96,6 +96,40 @@ def trap_optimal(height):
     return water_trapped
 
 
+#Date Solved: March 4th 2026
+"""
+Notes: Had to revisit the video and understand the problem from scratch. I get the idea of the two pointer algorithm but purely for an interview perspective I will drill into the O(n) memory solution. 
+Reason being, it's more intuitive and easy to explain to the intervener and not confuse myself. 
+"""
+def trap_revision(height):
+
+    n = len(height)
+
+    #Pre-computing the left max heights (Prefix algo)
+    left_max_height_so_far = 0
+    prefix_left_max_heights = [0] * n
+    for i in range(1, n): #skipping the first index as the min will be 0
+        left_max_height_so_far = max(left_max_height_so_far, height[i-1])
+        prefix_left_max_heights[i] = left_max_height_so_far
+
+    #Pre-computing the right max heights (Postfix algo)
+    right_max_height_so_far = 0
+    postfix_right_max_heights = [0] * n
+    for i in range(n-2, -1, -1): #skipping the last index as the max will be 0
+        right_max_height_so_far = max(right_max_height_so_far, height[i+1])
+        postfix_right_max_heights[i] = right_max_height_so_far
+
+    
+    #formula for calculating the trapped rain water: min(left_max, right_max) - height[i]
+    water_trapped_so_far = 0
+    for i in range(n):
+        water_trapped = min(prefix_left_max_heights[i], postfix_right_max_heights[i]) - height[i]
+        if water_trapped < 0:
+            continue
+        water_trapped_so_far += water_trapped
+    
+    return water_trapped_so_far
+
 
 #Testing
-print(trap_optimal(height=[0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trap_revision(height=[0,1,0,2,1,0,1,3,2,1,2,1]))
