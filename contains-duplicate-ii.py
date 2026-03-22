bf066383f133b5569f51870da519f6edac0f788f
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-22 14:34:47 -0500

    solved effeciently

--- contains-duplicate-ii.py
+++ contains-duplicate-ii.py
@@ -13,6 +13,22 @@ def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
 
 #Efficient way O(n)
 
+def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
+    window = set()
+
+    L = 0
+    for R in range(len(nums)):
+
+        if (R - L) + 1 > k + 1:
+            window.remove(nums[L])
+            L+=1
+
+        if nums[R] in window:
+            return True
+
+        window.add(nums[R])
+
+    return False
 
 
 
