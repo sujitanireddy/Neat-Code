7422bbc069bce969e455cf4f3a337ac56cf909c1
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-22 10:53:46 -0500

    contains-duplicate-ii.py solved effecintly

--- contains-duplicate-ii.py
+++ contains-duplicate-ii.py
@@ -1,6 +1,24 @@
-#Question -> https://leetcode.com/problems/contains-duplicate-ii/description/
+#Question -> https://neetcode.io/problems/contains-duplicate-ii/question?list=neetcode250
 
-class Solution:
-    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+#BruteForce Approach
+#Time Complexity: O(n**2)
+def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
+
+    for L in range(len(nums)):
+        for R in range(L+1, min(len(nums), L + k + 1)):
+            if nums[L] == nums[R]:
+                return True
+    
+    return False
+
+#Efficient way O(n)
+
+
+
+
+
         
-        
+
+
+
+print(containsNearbyDuplicate(nums=[1,2,1,2], k=3))
