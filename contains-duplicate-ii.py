33316c5e6b51218aac8c0dc83ba6ea7fe4a99c43
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-22 10:02:58 -0500

    contains duplicate 2 question added to pactice sliding window

---
+++ contains-duplicate-ii.py
@@ -0,0 +1,6 @@
+#Question -> https://leetcode.com/problems/contains-duplicate-ii/description/
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        
+        
