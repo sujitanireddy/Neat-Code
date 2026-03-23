7b04061b7a19fe1e6557973e177a1a7a68a09209
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-23 09:00:09 -0500

    solved using sliding window

--- maximum-length-of-subarray-such-that-all-elements-are-equal-in-the-subarray.py
+++ maximum-length-of-subarray-such-that-all-elements-are-equal-in-the-subarray.py
@@ -18,4 +18,24 @@ def maximum_length_of_subarray(nums = [4,2,2,2,1,1]):
 
     return max_length
 
-print(maximum_length_of_subarray())
+#with sliding window algorithm
+def maximum_length_of_subarray_sliding_window(nums = [4,2,2,2,1,1]):
+
+    L = 0
+    max_length = 0
+
+    for R in range(len(nums)):
+
+        if nums[L] != nums[R]:
+
+            L = R
+        
+        max_length = max(max_length, (R - L) + 1)
+    
+    return max_length
+    
+
+
+
+
+print(maximum_length_of_subarray_sliding_window())
