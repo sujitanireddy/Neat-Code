99879c3a37503cb244dd58607ed8f739105310e6
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-03-27 20:44:00 -0500

    solved top-k-elements using bucket sort

--- top-k-elements-in-list.py
+++ top-k-elements-in-list.py
@@ -2,6 +2,8 @@
 
 from collections import defaultdict
 
+
+#O(n log n) solution
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         
@@ -17,3 +19,37 @@ class Solution:
             output.append(sorted_hashmap[i][0])
 
         return output
+
+
+# Time Complexity: O(n) 
+# Space Complexity: O(n)
+def topKFrequent_efficient(nums: list[int], k: int) -> list[int]:
+
+    freq_map = {}
+    for num in nums:
+        freq_map[num] = freq_map.get(num, 0) + 1
+    
+    buckets = []
+    for _ in range(len(nums) + 1): #if we take 1 freq of each number the max freq buckets we would need are len(nums) + 1
+        buckets.append([])
+
+    for num, freq in freq_map.items():
+        buckets[freq].append(num)
+    
+    output = []
+    for i in range(len(buckets) -1 , 0, -1):
+        for num in buckets[i]:
+            output.append(num)
+        if len(output) == k:
+            return output
+
+
+
+
+
+
+
+
+
+
+print(topKFrequent_efficient(nums=[7,8,8,9,9,9], k=2))
